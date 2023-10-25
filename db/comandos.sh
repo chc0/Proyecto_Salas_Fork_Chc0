923ab14a90150eab6fb3962653fb6133fc953c87
AlvirElWapo <correodeandresalvir@gmail.com>
2023-10-24 23:51:08 -0600

    cambios en bdd

--- db/comandos.sh
+++ db/comandos.sh
@@ -1,11 +1,19 @@
- docker run -d --name contenedor_alvir2 -v ./db:/cosas_db alvirelwapo/proyecto_salas_bdd
-
+docker run -d --name **nombre del contenedor** -v "orígen local":"Orígen en contenedor"  alvirelwapo/proyecto_salas_bdd
+                                                    #SIN COMILLAS; SOLO LAS USO PARA EJEMPLIFICAR
 docker image ls 
 docker container ls 
 
+
+#Acceder a la shell de mariadb
 docker exec -ti **nombre contenedor** bash
 
-Contraseña root contenedor por default: 6327
+#Contraseña root contenedor por default: 6327
+
+#Después de su creación, el estado de éste se guarda, debemos activarlo con:
+docker container start **nombre del contenedor** 
+
+
+
 
 
 
